b94b55babd0bf2df588075bfd84b2c2288b770aa
Nathan Yam <nathancyam@gmail.com>
2014-05-04 21:13:46 +1000

    Fix the ANN tests

--- specs/ann-spec.js
+++ specs/ann-spec.js
@@ -2,36 +2,36 @@
  * Created by nathanyam on 14/04/2014.
  */
 
-var ann = require('../src/resources/ann');
-var expect = require('chai').expect;
+var AnnResource = require('../src/resources/ann'),
+    ann = new AnnResource(),
+    expect = require('chai').expect;
 
 describe('Anime News Network API', function () {
-    describe('#searchByName()', function () {
-        it('should search an anime and return a JSON object', function (done) {
-            ann.searchByName('Kill la Kill', function (err, result) {
+    describe('#search()', function () {
+        it('should search an anime by name and return a JSON object', function (done) {
+            ann.search({ name: 'Kill la Kill'}, function (err, result) {
                 expect(result).to.be.a('object');
                 done();
             });
         });
-        it('should give us an anime from Google if it could not be found', function (done) {
-            ann.searchByName('Sakurasou no Pet na Kanojo', function (err, result) {
-                if (ann.isEmpty(result)) {
-                    ann.handleEmptyResponse(result, function (err, result) {
-                        expect(result).to.be.a('object');
-                        expect(result).to.have.property('ann');
-                        expect(result.ann.anime).to.be.a('array');
-                        done();
-                    });
-                }
-            });
-        });
-    });
-    describe('#searchById()', function () {
-        it('should give a more detailed log of the anime by ID', function (done) {
-            ann.searchById(13310, function (err, result) {
+        it('should search an anime by ID and return a JSON detailed object', function (done) {
+            ann.search({ ann_id: 15101 }, function (err, result) {
                 expect(result).to.be.a('object');
                 expect(result).to.have.property('ann');
                 expect(result.ann.anime).to.be.a('array');
+                done();
+            });
+        });
+        it('should give us an anime from Google if it could not be found', function (done) {
+            ann.search('Sakurasou no Pet na Kanojo', function (err, result) {
+                expect(result).to.be.a('object');
+                done();
+            });
+        });
+        it('should give us multiple results', function (done) {
+            ann.search({ name: 'Fate' }, function (err, result) {
+                expect(result).to.be.a('array');
+                expect(result.length).to.be.greaterThan(1);
                 done();
             });
         });
