8a8b03fd94cb6ef8631aa44a9218be8cf7da1e69
Nathan Yam <nathancyam@gmail.com>
2014-05-07 22:55:35 +1000

    Update to use Express 4

--- src/app.js
+++ src/app.js
@@ -3,38 +3,26 @@
  */
 
 var express = require('express'),
-    http = require('http'),
-    path = require('path'),
-    mongoose = require('mongoose');
-
-var config = require('./config'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    config = require('./config'),
     Settings = require('./models/settings');
 
 var app = express();
-
 mongoose.connect('mongodb://localhost/anime:27017');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(bodyParser());
+app.use(express.static(__dirname + '/../public'));
 
-app.configure('development', function () {
-    app.use(express.errorHandler());
-});
-
+// Set routes
 require("./routes")(app);
 
-// === SERVER ===
-http.createServer(app).listen(app.get('port'), function () {
-    console.log('Initialising settings...');
-    Settings.init(config);
-    console.log('Express server listening on port ' + app.get('port'));
-});
+// Initialise the settings
+Settings.init(config);
+
+// Ready for requests!
+app.listen(app.get('port'));
