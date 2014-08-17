b77ab6a811c36e03871fc0d17b7bec1d11c3f67c
Nathan Yam <nathancyam@gmail.com>
2014-08-17 14:32:19 +1000

    Add close directive

--- src/frontend/notifications.js
+++ src/frontend/notifications.js
@@ -7,11 +7,17 @@ NotifyApp.factory('NotificationResource', ['$resource', function ($resource) {
 NotifyApp.controller('NotificationController', ['$scope','NotificationResource', function ($scope, NotificationResource) {
     this.notifications = [{ message: 'I am an example message' }];
     $scope.messages = this.notifications;
+
+    this.clearAll = function () {
+        $scope.messages = [];
+    };
 }]);
 
 NotifyApp.directive('notifications', function () {
     return {
         controller: 'NotificationController',
+        transclude: true,
+        scope: {},
         link: function (scope, elem, attrs) {
             elem.bind('click', function () {
                 scope.$apply(function () {
@@ -20,5 +26,19 @@ NotifyApp.directive('notifications', function () {
             });
         },
         templateUrl: 'animeapp/views/notifications.html'
+    };
+});
+
+NotifyApp.directive('notificationClear', function () {
+    return {
+        require: '^notifications',
+        restrict: 'E',
+        scope: {},
+        link: function (scope, elem, attrs, notificationCtrl) {
+            elem.bind('click', function () {
+                notificationCtrl.clearAll();
+            });
+        },
+        template: '<span>X<span'
     };
 });
