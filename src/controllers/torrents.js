d1fced50364418e97fabf5dd2fd9762b46833863
Nathan Yam <nathancyam@gmail.com>
2016-04-10 12:47:26 +1000

    Add human ETA on server

--- src/controllers/torrents.js
+++ src/controllers/torrents.js
@@ -5,8 +5,8 @@
 /*jslint node: true*/
 "use strict";
 
-
 const router = require('express').Router();
+const moment = require('moment');
 
 /**
  * Adds a torrent to the torrent server
@@ -38,7 +38,24 @@ router.post('/add', (req, res) => {
 
 router.post('/server', (req, res) => {
   const torrentDetails = req.body.torrentServer;
-  req.app.get('socket_handler').emit('torrent_server:listing', torrentDetails);
+  let torrents = torrentDetails.map(torrent => {
+    let now = moment();
+    let torrentEta = moment().add(torrent.eta, 'seconds');
+    let days = torrentEta.diff(now, 'days');
+    let hours = torrentEta.diff(now, 'hours');
+    let minutes = torrentEta.diff(now, 'minutes');
+    let seconds = torrentEta.diff(now, 'seconds');
+
+    days = days ? `${days} days` : '';
+    hours = hours ? `${hours} hours` : '';
+    minutes = minutes ? `${minutes} minutes` : '';
+    seconds = seconds ? `${seconds} seconds` : '';
+
+    let humanEta = [days, hours, minutes, seconds].join(' ');
+    return Object.assign({}, torrent, { humanEta });
+  });
+
+  req.app.get('socket_handler').emit('torrent_server:listing', torrents);
   return res.json({ status: 'SUCCESS', message: 'Torrent server details posted' });
 });
 
