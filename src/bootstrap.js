fce724a4796e3dff4ce8fa9c4e1071ecdacdf4b2
Nathan Yam <nathancyam@gmail.com>
2016-07-16 12:05:00 +1000

    Ensure  calls do not directly use

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -7,20 +7,56 @@
 const mongoose = require('mongoose');
 const path = require('path');
 
-module.exports = (app, httpServer) => {
-  const modelDirectory = path.join(__dirname, 'models');
-  const helperDirectory = path.join(__dirname, 'helpers');
+// Models
+const AnimeModel = require('./models/anime');
+const EpisodeModel = require('./models/episode');
+const NotificationModel = require('./models/subgroup');
+const DirectoryModel = require('./models/anime_directory');
+const UserModel = require('./models/user');
 
+// Helpers
+const ApiParserHelper = require('./helpers/api_parsers');
+const CacheHelper = require('./helpers/cache');
+const EpisodeHelper = require('./helpers/episode');
+const GoogleHelper = require('./helpers/google');
+const TorrentHelper = require('./helpers/torrents');
+
+const models = {
+  'anime': AnimeModel,
+  'episode': EpisodeModel,
+  'notification': NotificationModel,
+  'anime_directory': DirectoryModel,
+  'user': UserModel
+};
+
+const helpers = {
+  'api_parsers': ApiParserHelper,
+  'cache': CacheHelper,
+  'episode': EpisodeHelper,
+  'google': GoogleHelper,
+  'torrent': TorrentHelper
+};
+
+/**
+ * @param {Object} obj
+ * @param {String} type
+ * @param {String} alias
+ * @returns {Object|Function}
+ */
+const resolver = (obj, type, alias) => {
+  const moduleDef = obj[alias];
+  if (typeof moduleDef === 'undefined') {
+    throw new Error(`${type} ${alias} not specified.`);
+  }
+  return moduleDef;
+};
+
+module.exports = (app, httpServer) => {
   app.get('console')('Binding model helpers...');
   mongoose.connect(app.get('app_config').mongo);
 
-  app.getModel = modelString => {
-    return require(`${modelDirectory}/${modelString}`);
-  };
-
-  app.getHelper = helperString => {
-    return require(`${helperDirectory}/${helperString}`);
-  };
+  app.getModel = resolver.bind(null, models, 'Model');
+  app.getHelper = resolver.bind(null, helpers, 'Helper');
 
   app.get('console')('Binding services...');
   require('./services/registrar')(app, httpServer);
