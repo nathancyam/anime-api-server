191539cd0bcb41897b8de46d7599d68aa8e59836
Nathan Yam <nathancyam@gmail.com>
2017-08-06 11:57:18 +1000

    Update deploy script

--- deploy/prod.sh
+++ deploy/prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SRC_DIR="./src"
-DEST_DIR="/var/www/node-anime-directory/"
+SRC_DIR="./src/*"
+DEST_DIR="/var/www/node-anime-directory/src/"
 
 rsync -rvz --progress -e "ssh -p ${SSH_PORT}" $SRC_DIR $REMOTE_USER@$REMOTE_HOST:$DEST_DIR
