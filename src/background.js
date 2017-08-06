0484ef46eae11e26a8400a4cf2384ab87ba0d81d
Nathan Yam <nathancyam@gmail.com>
2017-08-06 11:49:52 +1000

    Update console.log for background.js

--- src/background.js
+++ src/background.js
@@ -3,6 +3,7 @@ const HOUR_INTERVAL = 1000 * 60 * 60;
 module.exports = (app) => {
   const commandManager = app.get('command');
   const bus = app.get('bus');
+  console.log(`${Date.now()} [Auto Update] Starting background timeout.`);
 
   setTimeout(() => {
     console.log('Updating anime');
