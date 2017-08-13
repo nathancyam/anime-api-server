9d8bd9379ff36e26be4f65845ff0e3ea6903aacc
Nathan Yam <nathancyam@gmail.com>
2017-08-13 14:40:16 +1000

    Try this again

--- deploy/prod.sh
+++ deploy/prod.sh
@@ -13,5 +13,5 @@ DEPLOY_CMD="cd /var/www/node-anime-directory/builds \
 && ln -nfs /var/www/node-anime-directory/current ."
 
 tar -zcvf $ARCHIVE_FILE_NAME --exclude=./.git --exclude=./*.tar.gz --exclude=./.idea --exclude=./node_modules --exclude=./tests --exclude=./media --exclude=./config.json .
-rsync -rvz --progress -e "ssh -p ${SSH_PORT}" $ARCHIVE_FILE_NAME $REMOTE_USER@$REMOTE_HOST:$DEST_DIR
-ssh -t -p $SSH_PORT $REMOTE_USER@$REMOTE_HOST $DEPLOY_CMD
+rsync -rvz --progress -e "ssh -p ${SSH_PORT}" $ARCHIVE_FILE_NAME $REMOTE_USER@$REMOTE_HOST:$DEST_DIR/
+ssh -p $SSH_PORT $REMOTE_USER@$REMOTE_HOST -t $DEPLOY_CMD
