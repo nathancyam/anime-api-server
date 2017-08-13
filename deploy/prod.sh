845222a15360409360608e6be5888f6a14f4f7eb
Nathan Yam <nathancyam@gmail.com>
2017-08-13 14:50:29 +1000

    I hate inconsistent case flags

--- deploy/prod.sh
+++ deploy/prod.sh
@@ -13,5 +13,5 @@ DEPLOY_CMD="cd /var/www/node-anime-directory/builds \
 && ln -nfs . /var/www/node-anime-directory/current"
 
 tar -zcvf /tmp/$ARCHIVE_FILE_NAME --exclude=./.git --exclude=./*.tar.gz --exclude=./.idea --exclude=./node_modules --exclude=./tests --exclude=./media --exclude=./config.json .
-scp -p ${SSH_PORT} /tmp/$ARCHIVE_FILE_NAME $REMOTE_USER@$REMOTE_HOST:$DEST_DIR/$ARCHIVE_FILE_NAME
+scp -P $SSH_PORT /tmp/$ARCHIVE_FILE_NAME $REMOTE_USER@$REMOTE_HOST:$DEST_DIR/$ARCHIVE_FILE_NAME
 ssh -p $SSH_PORT $REMOTE_USER@$REMOTE_HOST -t $DEPLOY_CMD
