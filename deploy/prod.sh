9a8d6948dc6e4ad026e76b06e1adf645634433b4
Nathan Yam <nathancyam@gmail.com>
2017-08-13 15:26:17 +1000

    Symlink release directory

--- deploy/prod.sh
+++ deploy/prod.sh
@@ -11,7 +11,7 @@ DEPLOY_CMD="cd /var/www/node-anime-directory/builds \
 && tar -zxvf $ARCHIVE_FILE_NAME -C $ARCHIVE_DIR \
 && cd $ARCHIVE_DIR \
 && yarn \
-&& ln -nfs . /var/www/node-anime-directory/current"
+&& ln -nfs /var/www/node-anime-directory/builds/$ARCHIVE_DIR /var/www/node-anime-directory/current"
 
 tar -zcvf /tmp/$ARCHIVE_FILE_NAME --exclude=./.git --exclude=./*.tar.gz --exclude=./.idea --exclude=./node_modules --exclude=./tests --exclude=./media --exclude=./config.json .
 scp -P $SSH_PORT /tmp/$ARCHIVE_FILE_NAME $REMOTE_USER@$REMOTE_HOST:$DEST_DIR/$ARCHIVE_FILE_NAME
