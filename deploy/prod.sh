3b425d31ab44f095010b428bda513128c0dba53f
Nathan Yam <nathancyam@gmail.com>
2017-08-13 14:32:54 +1000

    Create deploy script

--- deploy/prod.sh
+++ deploy/prod.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 SRC_DIR="./src"
-DEST_DIR="/var/www/node-anime-directory/"
+DEST_DIR="/var/www/node-anime-directory/builds"
+DATE=$(date +"%Y%m%d%H%M")
+ARCHIVE_DIR="api_server"_$(date +"%Y%m%d%H%M")
+ARCHIVE_FILE_NAME=$ARCHIVE_DIR.tar.gz
 
-rsync -rvz --progress -e "ssh -p ${SSH_PORT}" $SRC_DIR $REMOTE_USER@$REMOTE_HOST:$DEST_DIR
+DEPLOY_CMD="cd /var/www/node-anime-directory/builds \
+&& tar -zxvf $ARCHIVE_FILE_NAME \
+&& cd $ARCHIVE_DIR \
+&& yarn \
+&& ln -nfs /var/www/node-anime-directory/current ."
+
+echo tar -zcvf $ARCHIVE_FILE_NAME --exclude=./.git --exclude=./*.tar.gz --exclude=./.idea --exclude=./node_modules --exclude=./tests --exclude=./media --exclude=./config.json .
+echo rsync -rvz --progress -e "ssh -p ${SSH_PORT}" $ARCHIVE_FILE_NAME $REMOTE_USER@$REMOTE_HOST:$DEST_DIR
+echo ssh -t -p $SSH_PORT $REMOTE_USER@$REMOTE_HOST $DEPLOY_CMD
