87ba0c3bbc37182ca70e39686e19d3f4d31e1097
Nathan Yam <nathancyam@gmail.com>
2017-09-03 16:34:51 +1000

    ffs

--- src/background.js
+++ src/background.js
@@ -3,10 +3,9 @@ const HOUR_INTERVAL = 1000 * 60 * 60;
 module.exports = (app) => {
   const commandManager = app.get('command');
   const bus = app.get('bus');
-  console.log(`${Date.now()} [Auto Update] Starting background timeout.`);
 
-  setTimeout(() => {
-    console.log('Updating anime');
+  setInterval(() => {
+    console.log(`${Date.now()} [Auto Update] Starting background interval.`);
     const updateCommand = commandManager.create('anime_update', { is_watching: true });
     bus.handle(updateCommand);
   }, HOUR_INTERVAL * 3);
